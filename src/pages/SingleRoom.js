6875e17c53f908729f7d630dd6e313d4a03f9fe5
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-25 18:49:27 +0530

    single room hero

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import defaultBcg from "./../images/room-1.jpeg";
 import { RoomContext } from "./../context";
+import { Link } from "react-router-dom";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
 export class SingleRoom extends Component {
   constructor(props) {
     super(props);
@@ -14,8 +17,36 @@ export class SingleRoom extends Component {
   render() {
     let { getRoom } = this.context;
     const room = getRoom(this.state.slug);
-    console.log(room);
-    return <div>SingleRoom</div>;
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>No such room found</h3>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </div>
+      );
+    }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images,
+    } = room;
+    return (
+      <Hero hero="roomsHero">
+        <Banner title={`${name} room`}>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </Banner>
+      </Hero>
+    );
   }
 }
 
